e6f5504d985b33d5b524ac56bceed10979a39893
aishte05 <jamesrazzelr@gmail.com>
2023-11-17 15:01:34 +0800

    Initial creation

---
+++ src/act1/looping.java
@@ -0,0 +1,19 @@
+package act1;
+
+public class looping {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] matrix = new int[3][4];
+
+        // Initializing with values
+        int[][] matrixValues = { {1, 2, 3, 4},
+                                 {5, 6, 7, 8},
+                                 {9, 10, 11, 12} };
+
+        // Accessing and printing elements
+        System.out.println("Element at (1,2): " + matrixValues[1][2]); // Prints 7
+
+	}
+
+}
